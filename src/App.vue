c06da4540da76b06401adbc1ab4d7b352f8f8b0b
Alfatta Rezqa <alfatta@it.damcorp.id>
2022-10-26 09:49:17 +0700

    day 2 : conditional rendering multiple element

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,15 @@
   <p>Usia saya : {{ usia }} tahun</p>
   <input type="number" v-model.number="usia" />
 
-  <p v-if="usia < 20">
-    Usia anda dibawah 20 tahun ( pakai v-if )
-  </p>
-  <h1 v-else-if="usia == 31">
-    Usia anda 31 ya?
-  </h1>
-  <p :style="{color: 'purple'}" v-else-if="usia >= 20">
-    Usia anda diatas 20 tahun
-  </p>
-  <div>
-    <p v-if="usia == 900">ini coba coba</p>
-    <p v-else>
-      Usia anda diatas 20 tahun
-    </p>
-  </div>
+  <template v-if="usiaDiatas20">
+    <p>Selamat Pagi Pak</p>
+    <p>Usia anda diatas 20</p>
+  </template>
 
-  <p v-show="usia < 20">
-    Usia anda dibawah 20 tahun ( pakai v-show )
-  </p>
+  <template v-else>
+    <p>Selamat Pagi Kak</p>
+    <p>Usia anda tidak diatas 20</p>
+  </template>
 
   <p>Terima kasih</p>
 </template>
@@ -31,6 +21,11 @@ export default {
     return {
       usia: 20,
     };
+  },
+  computed: {
+    usiaDiatas20() {
+      return this.usia > 20;
+    },
   },
 };
 </script>
