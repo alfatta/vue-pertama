b2783a88c667b0413815d75d8f13c686a279f2b9
Alfatta Rezqa <alfatta@it.damcorp.id>
2022-10-26 15:55:03 +0700

    day 2 : refs

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,8 +1,21 @@
 <template>
 
-  <input type="text">
+  <input type="text" ref="username" v-model="username">
+  <input type="text" ref="password">
+
+  <p v-for="n in 10" ref="paragraf"> test </p>
+
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      username: 'test'
+    }
+  },
+  mounted() {
+    this.$refs.username.focus()
+  }
+}
 </script>
